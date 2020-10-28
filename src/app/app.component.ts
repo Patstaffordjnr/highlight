4687234efb786aafe6d3a4d3e9d375c572f1e75f
Patrick Stafford <patstaffordjnr@gmail.com>
2020-10-28 20:58:54 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { MediaComponent } from './media/media.component';
+
 
 @Component({
   selector: 'app-root',
