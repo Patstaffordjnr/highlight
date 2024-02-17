0cc30b8aaced4c38e8ad0d844946b8b5821517fb
Paul Stafford <paul.stafford@zalando.de>
2024-02-17 21:08:12 +0100

    adsads

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { JsonPipe, NgFor } from '@angular/common';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { FormArray } from '@angular/forms';
 import { SignUpService } from './sign-up.service';
 import { UserRole } from 'src/app/model/user-role';
 import { SignUpRequest } from './sign-up-request';
+import { minSelectedCheckboxes } from 'src/app/util/validators/checkbox-validator';
+
+// export type UserFormControls = {
+//   userRoles: FormControl<UserRole[]>;
+// };
+
+// export type UserFormGroup = FormGroup<UserFormControls>;
+
 
 @Component({
   selector: 'app-sign-up',
@@ -13,6 +21,7 @@ import { SignUpRequest } from './sign-up-request';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  form: FormGroup;
 
   allUserRoles = Object.keys(UserRole).filter((item) => {
       return isNaN(Number(item));
@@ -20,16 +29,25 @@ export class SignUpComponent implements OnInit {
 
   signUpRequest = new SignUpRequest('dumb@dumb.com', [UserRole.ADMIN,  UserRole.USER], 'dumb')
 
+  // userFormGroup: UserFormGroup = new FormGroup<UserFormControls>({
+  //   userRoles: new FormControl([UserRole.ADMIN]),
+  // });
 
-  signUpForm = this.formBuilder.group({
-    email: [this.signUpRequest.email, [Validators.required, Validators.email]],
-    password: [this.signUpRequest.password, [Validators.required, Validators.minLength(4)]],
-    roles: [this.signUpRequest.roles, new FormArray([], [Validators.required])],
-  });
+  
  
   constructor(private formBuilder: FormBuilder, private signUpService: SignUpService ) { 
+    this.form = this.formBuilder.group({
+      email: [this.signUpRequest.email, [Validators.required, Validators.email]],
+      password: [this.signUpRequest.password, [Validators.required, Validators.minLength(4)]],
+      userRoles: new FormArray([], minSelectedCheckboxes(1))
+    });
+    this.addCheckboxes();
   }
-  
+
+  private addCheckboxes() {
+    this.allUserRoles.forEach(userRole => this.userRolesFormArray.push(new FormControl(this.isChecked(UserRole[userRole]))));
+  }
+
   onUserRoleUpdate(event: Event): void {
     var checkBoxEvent = event.target as HTMLInputElement
     var roleClicked = UserRole[checkBoxEvent.value]
@@ -40,15 +58,20 @@ export class SignUpComponent implements OnInit {
     }
   }
 
-  ngOnInit(): void {
+  get userRolesFormArray() {
+    return this.form.controls.userRoles as FormArray;
   }
 
+  ngOnInit(): void {
+  }
 
   onSubmit() {
 
   }
 
   isChecked(userRole: UserRole): Boolean {
+    console.log(userRole)
     return this.signUpRequest.roles.includes(userRole)
   }
+  
 }
