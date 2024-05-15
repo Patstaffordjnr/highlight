04860c50d7839cf938b893fbff46c0e72ee6273a
patrickstafford <patstaffordjnr@gmail.com>
2024-05-15 14:04:25 +0100

    Progress bar now emitting time

--- src/app/components/progress-bar/progress-bar.component.ts
+++ src/app/components/progress-bar/progress-bar.component.ts
@@ -15,6 +15,7 @@ export class ProgressBarComponent implements OnInit {
   @Output() selectTimeEvent = new EventEmitter<String>();
 
   @Input() selectedTime: Date;
+
   date = new Date();
   currentHour = this.date.getHours();
   mm = this.date.getMinutes();
@@ -31,6 +32,10 @@ export class ProgressBarComponent implements OnInit {
   }
 
   ngAfterViewInit() {
+
+    if(this.selectedTime) {
+            this.onStartUpMoveDotToPosition(this.selectedTime);
+          }
  
   }
   emitTime(hour, minutes) {
@@ -39,7 +44,32 @@ export class ProgressBarComponent implements OnInit {
         this.selectTimeEvent.emit(time);
       }
 
-
+      onStartUpMoveDotToPosition(time) {
+            const timeString = time.toString().trim();
+            console.log(timeString);
+            // const [hourString, minuteString] = timeString.split(":");
+        
+            // const progressBarElement: HTMLElement = this.progressBarDiv.nativeElement;
+            // let hour = time.getHours();
+            // let minutes = Number(minuteString);
+            // let hourPercentOfX = (hour / 24) * 100
+        
+            // let divLeft = progressBarElement.offsetLeft;
+            // // console.log(divLeft);
+            // let divRight = (divLeft + progressBarElement.offsetWidth);
+            // // console.log(divRight);
+            // let divLength = (divRight - divLeft);
+            // let divLengthByTwentyFour = (divLength / 24);
+            // let div60 = divLengthByTwentyFour / 60;
+            // let divMinutes = div60 * minutes;
+        
+            // let dotX = ((divLength / 100) * hourPercentOfX);
+        
+            // const dotElement: HTMLElement = this.dot.nativeElement;
+            // dotElement.style.left =`${dotX - 10}px`
+        
+          }
+        
 
 
   onDragMoved($event: CdkDragMove<any>){
@@ -50,168 +80,66 @@ export class ProgressBarComponent implements OnInit {
     dotPosition = Math.max(0, Math.min(dotPosition, divRect.width));
     let percentage = (dotPosition / 330) * 100;
 
-    let hour = Math.trunc(.24 * percentage);
-    const totalTime = 86400; // Assuming 24 hours (adjust as needed)
-    let divLengthByTwentyFour = (342 / 24);
+    let divLengthByTwentyFour = (350 / 24);
     let div60 = divLengthByTwentyFour / 60;
     let minuteLength = div60;
     let totalCurrentMinutes = minuteLength * percentage;
     let minutesPercentage = totalCurrentMinutes - Math.floor(totalCurrentMinutes);
-    let minutes =  Math.trunc(60 * minutesPercentage);
+  
+    let hour = Math.floor(totalCurrentMinutes);
+    let minutes =  Math.floor(Number(Math.trunc(60 * minutesPercentage)))
 
-        if(hour == 0 && minutes == 0) {
-      this.mapTime = `00:00 AM`
-      this.emitTime('00', '00');
-    } else if(hour == 24 && minutes > 0) {
-      this.mapTime = `24:00 PM`
-      this.emitTime('00', '00');
-    }  else {
-    // -------------------------------------------------------------------------------
-    const injectedHour = hour < 10 ? `0${hour}` : String(hour);
-    const injectedMinutes = minutes < 10 ? `0${minutes}` : String(minutes);
-    //--------------------------------------------------------------------------------GEMINI 
-    if(Number(injectedHour) > 12) {
-      this.mapTime = `${injectedHour}:${injectedMinutes} PM`
-      this.emitTime(injectedHour, injectedMinutes);
-    } else {
-      this.mapTime = `${injectedHour}:${injectedMinutes} AM`
-      this.emitTime(injectedHour, injectedMinutes);
+    if(minutes < 5) {
+      minutes = 0;
     }
+    if(minutes  > 5  &&  minutes < 9) {
+      minutes = 5;
+    }
+    if(minutes  > 9  &&  minutes < 14) {
+      minutes = 10;
+    }
+    if(minutes  > 14  &&  minutes < 19) {
+      minutes = 15;
+    }
+    if(minutes  > 19  &&  minutes < 24) {
+      minutes = 20;
+    }
+    if(minutes  > 24  &&  minutes < 29) {
+      minutes = 25;
+    }
+    if(minutes  > 29  &&  minutes < 34) {
+      minutes = 30;
+    }
+    if(minutes  > 35  &&  minutes < 39) {
+      minutes = 35;
+    }
+    if(minutes  > 40  &&  minutes < 44) {
+      minutes = 40;
+    }
+    if(minutes  > 45  &&  minutes < 49) {
+      minutes = 45;
+    }
+    if(minutes  > 50  &&  minutes < 54) {
+      minutes = 50;
+    }
+    if(minutes  > 55  &&  minutes < 58) {
+      minutes = 55;
     }
 
+    if(minutes > 50) {
+      hour++
+      minutes = 0;
+    }
+
+    const time = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate(), hour, minutes, 0);
+
+    let injectedHour = time.getHours() <= 9 ? `0${hour}` : String(time.getHours());
+    let injectedMinutes = time.getMinutes() <= 9 ? `0${time.getMinutes()}` : String(time.getMinutes());
+    if(hour === 24){
+      injectedHour = "00";
+      injectedMinutes = "00";
+    }
+    this.emitTime(injectedHour, injectedMinutes);
+
+  }
 }
-      
-}
-
-
-
-// import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
-// import { CdkDragMove } from '@angular/cdk/drag-drop'; // Import CdkDragMove
-// import { GlobalDateAndTimeComponentService } from '../../util/global-date-and-time/global-date-and-time.service'
-
-// @Component({
-//   selector: 'app-progress-bar',
-//   templateUrl: './progress-bar.component.html',
-//   styleUrls: ['./progress-bar.component.css']
-// })
-// export class ProgressBarComponent implements OnInit {
-
-//   @ViewChild('progressBarDiv') progressBarDiv: ElementRef;
-//   @ViewChild('dot') dot: ElementRef;
-//   @Output() selectTimeEvent = new EventEmitter<String>();
-
-//   @Input() selectedTime: Date;
-//   date = new Date();
-//   currentHour = this.date.getHours();
-//   mm = this.date.getMinutes();
-//   ss = this.date.getSeconds();
-//   session = "AM";
-//   hours = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"];
-//   mapTime
-
-
-//   constructor() {
-//   }
-
-//   ngOnInit(): void {
-//   }
-
-//   ngAfterViewInit() {
-//     if(this.selectedTime) {
-//       this.onStartUpMoveDotToPosition(this.selectedTime);
-//     }
-//   }
-//   onStartUpMoveDotToPosition(time) {
-//     const timeString = time.toString().trim();
-//     const [hourString, minuteString] = timeString.split(":");
-
-//     const progressBarElement: HTMLElement = this.progressBarDiv.nativeElement;
-//     let hour = time.getHours();
-//     let minutes = Number(minuteString);
-//     let hourPercentOfX = (hour / 24) * 100
-
-//     let divLeft = progressBarElement.offsetLeft;
-//     // console.log(divLeft);
-//     let divRight = (divLeft + progressBarElement.offsetWidth);
-//     // console.log(divRight);
-//     let divLength = (divRight - divLeft);
-//     let divLengthByTwentyFour = (divLength / 24);
-//     let div60 = divLengthByTwentyFour / 60;
-//     let divMinutes = div60 * minutes;
-
-//     let dotX = ((divLength / 100) * hourPercentOfX);
-
-//     const dotElement: HTMLElement = this.dot.nativeElement;
-//     dotElement.style.left =`${dotX - 10}px`
-
-//   }
-
-//   emitTime(hour, minutes) {
-//     let time = `${hour}:${minutes}`
-//     console.log(hour, minutes);
-//     this.selectTimeEvent.emit(time);
-//   }
-
-
-//   clock(hour: number, minutes: number) {
-//     if(hour == 0 && minutes == 0) {
-//       this.mapTime = `00:00 AM`
-//       this.emitTime('00', '00');
-//     } else if(hour == 24 && minutes > 0) {
-//       this.mapTime = `24:00 PM`
-//       this.emitTime('00', '00');
-//     }  else {
-//     // -------------------------------------------------------------------------------
-//     const injectedHour = hour < 10 ? `0${hour}` : String(hour);
-//     const injectedMinutes = minutes < 10 ? `0${minutes}` : String(minutes);
-//     //--------------------------------------------------------------------------------GEMINI 
-//     if(Number(injectedHour) > 12) {
-//       this.mapTime = `${injectedHour}:${injectedMinutes} PM`
-//       this.emitTime(injectedHour, injectedMinutes);
-//     } else {
-//       this.mapTime = `${injectedHour}:${injectedMinutes} AM`
-//       this.emitTime(injectedHour, injectedMinutes);
-//     }
-//     }
-
-//   }
-
-  // onDragMoved(event: CdkDragMove<any>) {
-
-  //   let dotPosition = event.pointerPosition.x;
-  //   let divElement: HTMLDivElement = this.progressBarDiv.nativeElement;
-  //   let divLeft = divElement.offsetLeft;
-  //   let divRight = (divLeft + divElement.offsetWidth);
-  //   let hourLength = (330) / 24
-  //   let dotX = dotPosition - divLeft
-
-// console.log(`Dot position: ${dotPosition}`);
-// console.log(`Div Left: ${divLeft}`);
-// console.log(`Div Right: ${divRight}`);
-
-
-
-
-
-
-
-//     if(dotPosition < divLeft) {
-//       dotPosition = divLeft;
-//       let hour = 0;
-//       let minutes = 0;
-//       return this.clock( hour, minutes)
-//     } else if(dotPosition > divRight) {
-//       // console.log(`Too Much`);
-//       return
-//     } else {
-//       let xValueHour = dotX / hourLength;
-//       let hour = Math.trunc(xValueHour);
-//       let minutesToBeCalculated = String(xValueHour % 1);
-//       let minutesMinusDecimalPoint = minutesToBeCalculated.slice(minutesToBeCalculated.indexOf(".") + 1);
-//       let secondsPercentage = Number(minutesMinusDecimalPoint.substring(0, 2));
-//       let minutes = Math.trunc((secondsPercentage / 100) * 60)
-//       this.clock(hour, minutes)
-//     }
-//   }
-
-// }
--- src/app/pages/events/event-modal/event-modal.component.ts
+++ src/app/pages/events/event-modal/event-modal.component.ts
@@ -84,11 +84,18 @@ if(selecteCurrentEvent.length != 0) {
       this.onStartDateSelected(this.startAt);
       this.onFinishDateSelected(this.endAt);
 
-      let startAtHour = String(this.startAt).substring(11, 13);
-      let startAtMinute = String(this.startAt).substring(14, 16);
-      let startTime = `${startAtHour}:${startAtMinute}`
-      this.startDateAndTime = new Date(this.startDateAndTime.getFullYear(), this.startDateAndTime.getMonth(), this.startDateAndTime.getDate(), Number(startAtHour), Number(startAtMinute), 0)
-      console.log(this.startDateAndTime);
+      // let startAtHour = String(this.startAt).substring(11, 13);
+      // let startAtMinute = String(this.startAt).substring(14, 16);
+      // let startTime = `${startAtHour}:${startAtMinute}`
+      // console.log(startTime);
+      // this.startDateAndTime = new Date(this.startDateAndTime.getFullYear(), this.startDateAndTime.getMonth(), this.startDateAndTime.getDate(), Number(startAtHour), Number(startAtMinute), 0);
+      // console.log(this.startDateAndTime);
+
+      // let finishAtHour = String(this.endAt).substring(11, 13);
+      // let finishAtMinute = String(this.endAt).substring(14, 16);
+      // let finsishTime = `${finishAtHour}:${finishAtMinute}`;
+      // this.finishDateAndTime = new Date(this.finishDateAndTime.getFullYear(), this.finishDateAndTime.getMonth(), this.finishDateAndTime.getDate(), Number(finishAtHour), Number(finishAtMinute), 0);
+      // console.log(this.finishDateAndTime);
   
 
     }
@@ -143,11 +150,18 @@ const day = Number($event.substring(8, 10));
 
 
 onStartTimeSelected($event) {
-console.log($event);
+
+let hour = Number($event.substring(0, 2))
+let minute = Number($event.substring(3, 5))
+
+
+this.startDateAndTime = new Date(this.startDateAndTime.getFullYear(), this.startDateAndTime.getMonth(), this.startDateAndTime.getDate(), hour, minute, 0)
+
 
   
   }
 
+  
 
 onFinishDateSelected($event) {
 
@@ -163,6 +177,18 @@ const day = Number($event.substring(8, 10));
 
 }
 
+
+onFinishTimeSelected($event) {
+
+  let hour = Number($event.substring(0, 2))
+  let minute = Number($event.substring(3, 5))
+  
+  
+  this.finishDateAndTime = new Date(this.finishDateAndTime.getFullYear(), this.finishDateAndTime.getMonth(), this.finishDateAndTime.getDate(), hour, minute, 0)
+  
+  
+    
+    }
   
   onSubmit(checkoutForm) {
       console.log(checkoutForm);
