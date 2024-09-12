42c807d814ebe697c364c2ff0bfbf2f3c6de5103
PatStaffordJnr <patstaffordjnr@gmail.com>
2024-09-12 14:51:26 +0100

    a

---
+++ src/app/log-in/log-in.client.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { LoginRequest } from './login-request';
+import { User } from 'src/app/model/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class LoginClient {
+
+
+  constructor(private http: HttpClient) {
+  }
+
+  
+  async whoAmI(){    
+    let response = await this.http.get<any>("http://localhost:8085/user/whoAmI", { withCredentials: true }).toPromise();
+    console.log(response)
+  }
+
+  async logIn(loginRequest: LoginRequest): Promise<User>{    
+      let url =  'http://localhost:8085/api/auth/login' ;
+      let headers = new HttpHeaders({
+        'Content-Type': 'application/json'
+      });      
+      return await this.http.post<User>(url, JSON.stringify(loginRequest), { withCredentials: true, headers: headers}).toPromise();
+  }
+
+  async getForbidden(){    
+    let response = await this.http.get<any>("http://localhost:8085/open/getForbidden", { withCredentials: true }).toPromise();
+    console.log(response)
+    
+  }
+
+
+
+}
--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { LoginRequest } from './login-request';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-
+import { LoginClient } from './log-in.client';
+// import { RouterService } from ''
 
 @Component({
   selector: 'app-log-in',
@@ -11,12 +12,12 @@ import { HttpClient } from '@angular/common/http';
   // imports: [],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.css'
-})
+})  // await this.routerService.clearCookie();
 export class LogInComponent {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+  constructor(private formBuilder: FormBuilder, private loginClient: LoginClient) {
 
     this.form = this.formBuilder.group({
       email: ['busker@dumb.com'],
@@ -25,18 +26,44 @@ export class LogInComponent {
 
   }
 
-async onSubmit(){
-  console.log(this.form);
-  let a = await this.login(this.form.value).subscribe((response: any) => {
-    // console.log(response);
-  })
-console.log(a);
+// async onSubmit(){
+//   console.log(this.form);
+//   let a = await this.login(this.form.value).subscribe((response: any) => {
+//     // console.log(response);
+//   })
+// console.log(a);
+
+// }
+
+// login(data: { email: string; password: string }) {
+//   return this.http.post('http://localhost:8085/api/auth/login', data);
+// }
+
+
+ngOnInit(): void {
 
 }
 
-login(data: { email: string; password: string }) {
-  return this.http.post('http://localhost:8085/api/auth/login', data);
+generateRequest(): LoginRequest {
+  return new LoginRequest(this.form.get('email').value, this.form.get('password').value)
 }
+
+async onSubmit() {
+  var loggedInUser = await this.loginClient.logIn(this.generateRequest());
+  // if(loggedInUser) {
+  //   this.routerService.setUser(loggedInUser);
+  //   this.routerService.toHomePage();
+  // }  
+}
+
+async getForbidden() {
+  await this.loginClient.getForbidden();
+}
+
+async clearCookie() {
+  // await this.routerService.clearCookie();
+}
+
 
 
 }
