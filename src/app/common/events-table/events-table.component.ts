0f17d5d45349591494d70b55a837d3893d16046d
PatStaffordJnr <patstaffordjnr@gmail.com>
2024-10-19 20:21:01 +0100

    a

--- src/app/common/events-table/events-table.component.ts
+++ src/app/common/events-table/events-table.component.ts
@@ -10,15 +10,6 @@ import { EventsClient } from '../event/events-client';
 export class EventsTableComponent implements OnInit {
 
 
-  currentIndex = 0;
-  reveivedObject
-
-  noOfEventsPerPage = 10;
-
-  eventResponseList: PageListResponse = {
-    total: 0,
-   results: []
-  };
 
 constructor (private eventsClient: EventsClient) {
 
@@ -26,27 +17,7 @@ constructor (private eventsClient: EventsClient) {
 
 async ngOnInit() { 
 
-  this.currentIndex = this.currentIndex;
 
-
-
-  let initialEventList =   await this.eventsClient.getEvents(this.currentIndex, this.noOfEventsPerPage)
-
-
-  this.reveivedObject =  initialEventList;
-  this.eventResponseList.total = this.reveivedObject.total;
-  this.eventResponseList.results = this.reveivedObject.results;
-
-  console.log(this.eventResponseList);
-
-//   this.googleMapService.updateEventsToBeDisplayed(this.eventResponseList.results);
-
-//  this.reveivedObject.results.forEach(event => {
-//   this.addressList(event.lat, event.long);
-// })
-
-
-//   this.pageNumberOrchestration(this.reveivedObject.results.length, this.reveivedObject.total, this.currentIndex)
 }
 
 }
