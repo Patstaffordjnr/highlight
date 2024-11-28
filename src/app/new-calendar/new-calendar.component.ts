826aff0e963bceb498d509894b50abea587608c3
PatStaffordJnr <patstaffordjnr@gmail.com>
2024-11-28 19:57:46 +0000

    Paul is not cool

---
+++ src/app/new-calendar/new-calendar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-calendar',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-calendar.component.html',
+  styleUrl: './new-calendar.component.css'
+})
+export class NewCalendarComponent {
+
+}
