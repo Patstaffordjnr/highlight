7442498f8b6a8013d7ec6e82de0d082a366d8b80
PatStaffordJnr <patstaffordjnr@gmail.com>
2025-10-09 21:33:57 +0100

    KLEAN WORKING TREE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,6 @@ import { NavComponent } from './common/nav/nav.component';
 import { LogInComponent } from './pages/log-in/log-in.component';
 import { HomeComponent } from './pages/home/home.component';
 import { UserComponent } from './pages/user/user.component';
-import { UsersComponent } from './pages/users/users.component';
-import { EditEventsComponent } from './pages/edit-events/edit-events.component';
-import { EditUsersComponent } from './pages/edit-users/edit-users.component';
 import { CalendarComponent } from './common/calendar/calendar.component';
 import { EventsComponent } from './pages/events/events.component';
 import { BuskerComponent } from './pages/busker/busker.component';
@@ -24,9 +21,6 @@ const routes: Routes = [
   { path: 'login', component: LogInComponent },
   { path: 'mainmenu', component: NavComponent },
   { path: 'user', component: UserComponent},
-  { path: 'users', component: UsersComponent},
-  { path: 'editevents', component: EditEventsComponent},
-  { path: 'editusers', component: EditUsersComponent},
   { path: 'calendar', component: CalendarComponent},
   { path: 'busker', component: BuskerComponent},
   { path: 'admin', component: AdminComponent},
